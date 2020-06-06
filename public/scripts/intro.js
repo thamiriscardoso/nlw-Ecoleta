564634139b7be8a50f367bad2b9f37f6b12973dd
Thamiris Cardoso <47259135+thamiriscardoso@users.noreply.github.com>
2020-06-06 20:41:28 -0300

    Criação do servidor, das rotas e utilização de nunjucks
    Criação do servidor e instalação de dependências;
    Reorganização das pastas e configuração da pasta public
    Criação de rotas;
    Utilização de Template engine (Nunjucks).
    
    **Alteração do form Create point para inserção de imagem da entidade
    **Criação do Arquivo Layout para facilitar edição das partes padrões do projeto

---
+++ public/scripts/intro.js
@@ -0,0 +1,67 @@
+//coments
+//document.write("Hello") 
+
+//variáveis
+/*var myvar = "He"
+const myconst = "He"
+document.write(myconst + myvar)*/
+
+/*string
+const s1 = "Isso é uma String";
+const s2 = 'Isso também é uma String';
+document.write(s2)*/
+
+/*number
+const n1 =1
+const n2 =12
+document.write(n1 + n2)*/
+
+/*boolean  true/false
+const bTrue = true
+const bFalse = false
+document.write(bFalse);*/
+
+//objeto possuem propriedade e funcionalidade
+
+/*const pessoa = {
+  altura: "1,80m",
+  idade: 24,
+  solteiro: true,
+  correr(){
+    return" Run Forest"
+  }
+}
+document.write(pessoa.correr())*/
+
+/*array - vetores
+const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Name"}]
+
+document.write(colecaoDeBolinhas)
+document.write(colecaoDeBolinhas[3].name)*/
+
+/*Funções - Funcionalidade - Atalhos - Reuso de Código
+
+//registrar função
+function sayMyName(name){
+  document.write(name)
+}
+ //executar
+sayMyName("Douglas")
+sayMyName("Tiago")
+sayMyName("Thamiris")*/
+
+/*condicionais
+
+const notaFinal = 7
+
+if(notaFinal < 5 ){
+  document.write("Reprovado")
+}else{
+  document.write("Aprovado")
+}*/
+
+/*Repetição - loop
+
+for (i = 0;i < 10; i++){
+  document.write(`<p>${i}</p>`)
+}*/
